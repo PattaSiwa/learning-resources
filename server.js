94917fc34240f9ee54d7e18140bfd98100176ea4
Thay <pattasiwa@gmail.com>
2021-03-22 13:48:18 -0400

    added 404 page, starting authentication with passport

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express()
 require('dotenv').config()
 const methodOverride = require('method-override')
 const PORT = process.env.PORT
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 
 //database
@@ -23,11 +27,32 @@ db.on('error', (err) => { console.log('ERROR: ', err) });
 db.on('connected', () => { console.log("mongo connected") })
 db.on('disconnected', () => { console.log("mongo disconnected") })
 
+//session config
+const sessionConfig = {
+    secret: "secretsecrethehe",
+    resave: false,
+    saveUninitialized: true,
+    cooke: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
 
 //middlewares
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+app.use(session(sessionConfig))
+
+//authentication
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
 
 //controllers
 
@@ -40,6 +65,9 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
+app.use((req, res) => {
+    res.status(404).render('notfound.ejs')
+})
 
 app.listen(PORT, () => {
     console.log('Listening on ', PORT)
