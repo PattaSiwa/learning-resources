12db26ffd4dfca50c72d8711d76c7ef8fd1b5485
Thay <pattasiwa@gmail.com>
2021-03-24 21:53:00 -0400

    session back on

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
-// app.use(session(sessionConfig))
+app.use(session(sessionConfig))
 app.use(flash())
 
 
